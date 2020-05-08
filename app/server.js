a45408c5c585912c13ac4f8a18415079fc5e65ed
Adalberto Zanata <zanata@gmail.com>
2020-05-08 13:54:54 -0300

    Adiciona o nunjucks e inicia o fatiamento do template

--- app/server.js
+++ app/server.js
@@ -5,13 +5,20 @@ const server = express();
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"));
 
+// confgurar o nunjucks
+const nunjucks = require('nunjucks');
+nunjucks.configure("views",{
+    express: server,
+    noCache: true  // em dev é bom desabilitar
+});
+
 // criação da rota "/"
 server.get("/", function(req, res) {
-    return res.sendFile(__dirname + "/index.html");
+    return res.render("index.html");
 });
 
 server.get("/proposals", function(req, res) {
-    return res.sendFile(__dirname + "/sugestoes.html");
+    return res.render("sugestoes.html");
 });
 
 // server ativo no port TCP 3000
