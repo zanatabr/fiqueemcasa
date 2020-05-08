3ae083b5045421db84ca71ae51539d1781f6267d
Adalberto Zanata <zanata@gmail.com>
2020-05-08 15:33:01 -0300

    Faz com que a apresentação das sugestões seja feita dinamicamente

--- app/server.js
+++ app/server.js
@@ -2,6 +2,37 @@
 const express = require("express");
 const server = express();
 
+const proposals = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2877/2877881.svg",
+        title: "Cursos de Programação",
+        category: "Estudos",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quisquam sapiente mollitia sequi aliquam necessitatibus laudantium eum qui enim veritatis...",
+        url:"http://www.dfdx.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/753/753024.svg",
+        title: "Exercícios Físicos",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quisquam sapiente mollitia sequi aliquam necessitatibus laudantium eum qui enim veritatis...",
+        url:"http://www.dfdx.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2436/2436636.svg",
+        title: "Clube de Leitura",
+        category: "Livros",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quisquam sapiente mollitia sequi aliquam necessitatibus laudantium eum qui enim veritatis...",
+        url:"http://www.dfdx.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/753/753024.svg",
+        title: "Meditação",
+        category: "Estudos",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quisquam sapiente mollitia sequi aliquam necessitatibus laudantium eum qui enim veritatis...",
+        url:"http://www.dfdx.com.br"
+    }
+];
+
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"));
 
@@ -14,11 +45,14 @@ nunjucks.configure("views",{
 
 // criação da rota "/"
 server.get("/", function(req, res) {
-    return res.render("index.html");
+    //let lastProposals = proposals.slice(-2).reverse();
+    const lastProposals = [...proposals].slice(-2).reverse();
+    
+    return res.render("index.html", {proposals: lastProposals} );
 });
 
 server.get("/proposals", function(req, res) {
-    return res.render("sugestoes.html");
+    return res.render("sugestoes.html", {proposals: [...proposals].reverse()});
 });
 
 // server ativo no port TCP 3000
